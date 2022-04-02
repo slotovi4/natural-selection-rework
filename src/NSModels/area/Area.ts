d3cb86446c7d1f0a08cf3c986c7062effc761766
Bruhns Roman <roman.bruns@mail.ru>
2022-04-02 16:53:28 +0300

    feat: addborderSize to Area model

--- src/NSModels/area/Area.ts
+++ src/NSModels/area/Area.ts
@@ -1,16 +1,24 @@
 import { CanvasElement } from '../CanvasElement';
 
 export abstract class Area extends CanvasElement {
-	public constructor(props: TCanvasElementProps) {
+	private readonly _borderSize: IProps['borderSize'];
+
+	public constructor(props: IProps) {
 		super(props);
+
+		this._borderSize = props.borderSize;
 	}
 
 	protected draw() {
 		this._ctx.beginPath();
-		this._ctx.rect(0, 0, this._width, this._height);
-		this._ctx.stroke();
+		this._ctx.lineWidth = this._borderSize;
+		this._ctx.strokeRect(0, 0, this._width, this._height);
 		this._ctx.closePath();
 	}
 }
 
 type TCanvasElementProps = ConstructorParameters<typeof CanvasElement>[0]
+
+interface IProps extends TCanvasElementProps {
+	borderSize: number;
+}
