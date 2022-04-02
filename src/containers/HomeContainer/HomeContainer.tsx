75aaddf053a1afab95ffb31d6a1dfb88001b0d23
Bruhns Roman <roman.bruns@mail.ru>
2022-04-02 16:54:31 +0300

    feat: update HomeContainer

--- src/containers/HomeContainer/HomeContainer.tsx
+++ src/containers/HomeContainer/HomeContainer.tsx
@@ -4,13 +4,14 @@ import { NaturalSelectionConstructor } from 'src/NSModels';
 
 export const HomeContainer = () => {
 	const areaSize = 500;
+	const areaBorderSize = 1;
 	const areaCanvasRef = useRef<HTMLCanvasElement | null>(null);
 
 	useEffect(() => {
 		const areaCtx = areaCanvasRef.current?.getContext('2d');
 
 		if (areaCtx) {
-			const ns = new NaturalSelectionConstructor({ areaCtx, areaSize });
+			const ns = new NaturalSelectionConstructor({ areaCtx, areaSize, areaBorderSize });
 		}
 	}, []);
 
