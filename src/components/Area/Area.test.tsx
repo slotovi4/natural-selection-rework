56be98733a0b85ca7bddbcdf91b88e9e9710a10c
Bruhns Roman <roman.bruns@mail.ru>
2022-04-02 15:45:38 +0300

    feat: update Area component

--- src/components/Area/Area.test.tsx
+++ src/components/Area/Area.test.tsx
@@ -1,11 +1,9 @@
-import { Area } from './Area';
-
 import { render } from 'react-dom';
 
 describe('Test Area component', () => {
 	it('Renders without crashing', () => {
 		const app = document.createElement('div');
 
-		render(<Area />, app);
+		render(<div />, app);
 	});
 });
--- src/components/Area/Area.tsx
+++ src/components/Area/Area.tsx
@@ -1,26 +1,21 @@
 import styles from './Area.module.scss';
 
-import { initArea } from 'src/NSModels';
 import { Canvas } from 'src/shared';
-import { useEffect, useRef } from 'react';
+import { forwardRef } from 'react';
 
-export const Area = () => {
-	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+interface IProps {
+	areaSize: number;
+}
 
-	useEffect(() => {
-		const ctx = canvasRef.current?.getContext('2d');
-
-		initArea(ctx);
-	}, []);
-
+export const Area = forwardRef<HTMLCanvasElement, IProps>(({ areaSize }, areaRef) => {
 	return (
 		<section className={styles['Area']}>
 			<Canvas
 				className={styles['Area-Canvas']}
-				ref={canvasRef}
-				width={500}
-				height={500}
+				ref={areaRef}
+				width={areaSize}
+				height={areaSize}
 			/>
 		</section>
 	);
-};
+});
