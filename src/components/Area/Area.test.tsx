0c44103bcbd4e2538cca8635d821089c7db210a7
Bruhns Roman <roman.bruns@mail.ru>
2022-04-03 14:28:09 +0300

    refactor: remove Area component

--- src/components/Area/Area.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from 'react-dom';
-
-describe('Test Area component', () => {
-	it('Renders without crashing', () => {
-		const app = document.createElement('div');
-
-		render(<div />, app);
-	});
-});
