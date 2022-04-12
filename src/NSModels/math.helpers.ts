070153bba5d0c543e52402cdf83ff6dadb4928d2
Bruhns Roman <roman.bruns@mail.ru>
2022-04-12 16:14:51 +0300

    feat: add randomBoolean math helper

--- src/NSModels/math.helpers.ts
+++ src/NSModels/math.helpers.ts
@@ -6,6 +6,8 @@ export const randomValue = ({ min, max }: IRandomValue) => {
 
 export const positiveOrNegativeValue = () => Math.random() > 0.5 ? 1 : -1;
 
+export const randomBoolean = () => Math.random() >= 0.5;
+
 export const degreesToRadians = (degrees: number) => degrees * (Math.PI / 180);
 
 export const radiansToDegrees = (radians: number) => radians * (180 / Math.PI);
