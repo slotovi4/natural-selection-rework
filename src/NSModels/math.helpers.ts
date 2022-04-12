e0a9f2e7254c31ff2e3bb53ec36de54aabf77cb9
Bruhns Roman <roman.bruns@mail.ru>
2022-04-12 15:43:50 +0300

    feat: add new math helpers

--- src/NSModels/math.helpers.ts
+++ src/NSModels/math.helpers.ts
@@ -4,6 +4,12 @@ export const randomValue = ({ min, max }: IRandomValue) => {
 	return Number(value.toFixed(2));
 };
 
+export const positiveOrNegativeValue = () => Math.random() > 0.5 ? 1 : -1;
+
+export const degreesToRadians = (degrees: number) => degrees * (Math.PI / 180);
+
+export const radiansToDegrees = (radians: number) => radians * (180 / Math.PI);
+
 interface IRandomValue {
 	min: number;
 	max: number;
