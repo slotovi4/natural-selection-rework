38c346a8fc17a2260ee690489da3979cbfb05368
Bruhns Roman <roman.bruns@mail.ru>
2022-04-12 16:54:28 +0300

    feat: remove border from Area

--- src/NSModels/area/Area.ts
+++ src/NSModels/area/Area.ts
@@ -1,24 +1,18 @@
 import { CanvasElement } from '../CanvasElement';
 
 export abstract class Area extends CanvasElement {
-	private readonly _borderSize: IProps['borderSize'];
 
-	public constructor(props: IProps) {
+	public constructor(props: TCanvasElementProps) {
 		super(props);
-
-		this._borderSize = props.borderSize;
 	}
 
 	protected draw() {
 		this._ctx.beginPath();
-		this._ctx.lineWidth = this._borderSize;
+		this._ctx.lineWidth = 0;
+		this._ctx.strokeStyle = 'transparent';
 		this._ctx.strokeRect(0, 0, this._width, this._height);
 		this._ctx.closePath();
 	}
 }
 
-type TCanvasElementProps = ConstructorParameters<typeof CanvasElement>[0]
-
-interface IProps extends TCanvasElementProps {
-	borderSize: number;
-}
+type TCanvasElementProps = ConstructorParameters<typeof CanvasElement>[0];
