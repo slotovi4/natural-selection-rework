ed8e0f7c9ed5a82b9494154e1e039d5eedf35437
Bruhns Roman <roman.bruns@mail.ru>
2022-04-22 17:43:40 +0300

    perf: add toFixed to math helpers

--- src/NSModels/math.helpers.ts
+++ src/NSModels/math.helpers.ts
@@ -8,13 +8,11 @@ export const positiveOrNegativeValue = () => randomBoolean() ? 1 : -1;
 
 export const randomBoolean = () => Math.random() >= 0.5;
 
-export const degreesToRadians = (degrees: number) => degrees * (Math.PI / 180);
+export const degreesToRadians = (degrees: number) => Number((degrees * (Math.PI / 180)).toFixed(4));
 
-export const radiansToDegrees = (radians: number) => radians * (180 / Math.PI);
+export const radiansToDegrees = (radians: number) => Number((radians * (180 / Math.PI)).toFixed(4));
 
-export const calcDistance = (fPoint: IPoint, sPoint: IPoint) => {
-	return Math.hypot(fPoint.x - sPoint.x, fPoint.y - sPoint.y);
-};
+export const calcDistance = (fPoint: IPoint, sPoint: IPoint) => Number(Math.hypot(fPoint.x - sPoint.x, fPoint.y - sPoint.y).toFixed(4));
 
 interface IRandomValue {
 	min: number;
