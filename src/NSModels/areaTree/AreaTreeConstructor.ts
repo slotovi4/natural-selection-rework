a5d93882d3c2dbd0c3893f1ec04a0dcf9da2c985
Bruhns Roman <roman.bruns@mail.ru>
2022-04-23 20:20:35 +0300

    feat: perf AreaTreeConstructor && add areaBorderPoints draw

--- src/NSModels/areaTree/AreaTreeConstructor.ts
+++ src/NSModels/areaTree/AreaTreeConstructor.ts
@@ -1,38 +1,73 @@
 import { AreaTree } from './AreaTree';
 import { SectorConstructor } from './sector';
+import { PointConstructor } from './point';
+import { createAreaBorderPoints } from './helpers';
 
+import type { IPoint, ISectorPosition } from './helpers';
 import type { ISector } from './AreaTree';
 
 export class AreaTreeConstructor extends AreaTree {
 	private readonly _ctx: IProps['ctx'];
+	private readonly _sectorsNestingLevel = 4;
+	private readonly _areaSectorPosition: ISector['position'];
+
+	private _areaBorderPoints: IPoint[] = [];
+	private _rootSectorsList: ISector[] = [];
 
 	public constructor(props: IProps) {
 		super(props);
 
 		this._ctx = props.ctx;
+		this._areaSectorPosition = {
+			startPoint: { x: 0, y: 0 },
+			endPoint: { x: this._areaSize, y: this._areaSize }
+		};
 
 		this.init();
 	}
 
 	public draw() {
+		this.drawAreaSectors();
+		this.drawAreaBorderPoints();
+	}
+
+	private drawAreaSectors() {
 		this.drawSectors(this._rootSectorsList);
 	}
 
-	private init() {
-		const startPoint = { x: 0, y: 0 };
-		const endPoint = { x: this._areaSize, y: this._areaSize };
+	private drawAreaBorderPoints() {
+		this._areaBorderPoints.forEach(point => this.drawBorderPoint(point));
+	}
 
-		this._rootSectorsList = this.createSector({ startPoint, endPoint }, 4);
+	private init() {
+		this.initRootSectorsList();
+		this.initAreaBorderPoints();
+	}
+
+	private initRootSectorsList() {
+		this._rootSectorsList = this.createSector(this._areaSectorPosition, this._sectorsNestingLevel);
+	}
+
+	private initAreaBorderPoints() {
+		this._areaBorderPoints = createAreaBorderPoints(this._areaSectorPosition, Math.pow(this._sectorsNestingLevel, 2));
 	}
 
 	private drawSectors(sectorsList: ISector[]) {
-		sectorsList.forEach(sector => {
-			new SectorConstructor({ ctx: this._ctx, sector: sector.position });
+		sectorsList.forEach(({ position, subSectors }) => {
+			this.drawSector(position);
 
-			if (sector.subSectors) {
-				this.drawSectors(sector.subSectors);
+			if (subSectors && subSectors.length) {
+				this.drawSectors(subSectors);
 			}
 		});
+	}
+
+	private drawBorderPoint(point: IPoint) {
+		new PointConstructor({ ctx: this._ctx, point });
+	}
+
+	private drawSector(sector: ISectorPosition) {
+		new SectorConstructor({ ctx: this._ctx, sector });
 	}
 }
 
