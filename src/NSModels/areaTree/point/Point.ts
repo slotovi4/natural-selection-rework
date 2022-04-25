57cdd427e4e788b2a79b19fc4c9b758d81190d6c
Bruhns Roman <roman.bruns@mail.ru>
2022-04-25 18:59:29 +0300

    feat: add color to Point

--- src/NSModels/areaTree/point/Point.ts
+++ src/NSModels/areaTree/point/Point.ts
@@ -2,17 +2,21 @@ import { CanvasElement } from 'src/NSModels/CanvasElement';
 
 export abstract class Point extends CanvasElement {
 	private readonly _point: IProps['point'];
+	private readonly _color: IProps['color'];
 
 	public constructor(props: IProps) {
 		super(props);
 
 		this._point = props.point;
+		this._color = props.color;
 	}
 
 	protected draw() {
 		this._ctx.beginPath();
 		this._ctx.arc(this._point.x, this._point.y, 10, 0, Math.PI * 2);
+		this._ctx.fillStyle = this._color || 'black';
 		this._ctx.stroke();
+		this._ctx.fill();
 		this._ctx.closePath();
 	}
 }
@@ -20,6 +24,7 @@ export abstract class Point extends CanvasElement {
 interface IProps {
 	ctx: CanvasRenderingContext2D;
 	point: IPoint;
+	color?: string;
 }
 
 interface IPoint {
